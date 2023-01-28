520a610cb593bb36e5816b1231d0b94ac1a719e5
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-01-28 14:37:06 +0100

    Add reult logic

--- app.js
+++ app.js
@@ -8,9 +8,40 @@ choices.forEach(choice => choice.addEventListener('click',  (e) => {
     chosen = e.target.id;
     userChoice.innerHTML = chosen;
     generateComputerChoice();
+    getResult();
 }));
 
 function generateComputerChoice() {
     const randomNumber = Math.floor(Math.random() * choices.length);
     computerChoice.innerHTML = choices[randomNumber].id;
+}
+
+function getResult() {
+    if (computerChoice.innerHTML === userChoice.innerHTML) {
+        result.innerHTML = `It's a draw!`;
+    }
+
+    if (computerChoice.innerHTML === 'rock' && userChoice.innerHTML === 'paper') {
+        result.innerHTML = `Computer wins`;
+    }
+
+    if (computerChoice.innerHTML === 'rock' && userChoice.innerHTML === 'scissors') {
+        result.innerHTML = `Computer wins`;
+    }
+
+    if (computerChoice.innerHTML === 'paper' && userChoice.innerHTML === 'scissors') {
+        result.innerHTML = `You win`;
+    }
+
+    if (computerChoice.innerHTML === 'paper' && userChoice.innerHTML === 'rock') {
+        result.innerHTML = `You win`;
+    }
+
+    if (computerChoice.innerHTML === 'scissors' && userChoice.innerHTML === 'rock') {
+        result.innerHTML = `You win`;
+    }
+
+    if (computerChoice.innerHTML === 'scissors' && userChoice.innerHTML === 'paper') {
+        result.innerHTML = `Computer wins`;
+    }
 }
