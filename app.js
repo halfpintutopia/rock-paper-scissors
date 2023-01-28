641004052587e819b7ed1c79090b9105746d89a2
Sirinya Richardson <sirinya@lemonbrain.ch>
2023-01-28 14:15:41 +0100

    Add logic for computer choice

--- app.js
+++ app.js
@@ -4,8 +4,25 @@ const result = document.getElementById('result');
 let chosen;
 
 const choices = document.querySelectorAll('.button');
-console.log(choices)
 choices.forEach(choice => choice.addEventListener('click',  (e) => {
     chosen = e.target.id;
     userChoice.innerHTML = chosen;
-}))
+    generateComputerChoice();
+}));
+
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * choices.length);
+    console.log(randomNumber)
+
+    if (randomNumber === 0) {
+        computerChoice.innerHTML = choices[randomNumber].id;
+    }
+
+     if (randomNumber === 1) {
+        computerChoice.innerHTML = choices[randomNumber].id;
+    }
+
+      if (randomNumber === 2) {
+        computerChoice.innerHTML = choices[randomNumber].id;
+    }
+}
